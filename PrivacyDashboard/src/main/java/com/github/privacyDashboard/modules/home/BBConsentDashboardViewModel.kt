f57db5d783c78277b8a4b1698b268fcdf886658a
milan <milan@igrant.io>
2023-10-11 09:19:30 +0530

    Fix: #92 - Implemented view model in dashboard screen

---
+++ PrivacyDashboard/src/main/java/com/github/privacyDashboard/modules/home/BBConsentDashboardViewModel.kt
@@ -0,0 +1,10 @@
+package com.github.privacyDashboard.modules.home
+
+import com.github.privacyDashboard.modules.base.BBConsentBaseViewModel
+
+class BBConsentDashboardViewModel() : BBConsentBaseViewModel() {
+
+    init {
+        isLoading.value = true
+    }
+}
