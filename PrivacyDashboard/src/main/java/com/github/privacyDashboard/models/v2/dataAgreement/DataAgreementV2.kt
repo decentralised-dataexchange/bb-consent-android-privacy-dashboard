b65b787c3ace04760b3bc8080b68fbb6070209b7
milan <milan@igrant.io>
2023-10-25 17:36:22 +0530

    Add: #140 - Migrated the organization detail and data agreements to V2

---
+++ PrivacyDashboard/src/main/java/com/github/privacyDashboard/models/v2/dataAgreement/DataAgreementV2.kt
@@ -0,0 +1,25 @@
+package com.github.privacyDashboard.models.v2.dataAgreement
+
+import com.google.gson.annotations.SerializedName
+
+data class DataAgreementV2(
+
+    @SerializedName("id") var id: String? = null,
+    @SerializedName("version") var version: String? = null,
+    @SerializedName("controllerId") var controllerId: String? = null,
+    @SerializedName("controllerUrl") var controllerUrl: String? = null,
+    @SerializedName("controllerName") var controllerName: String? = null,
+    @SerializedName("policy") var policy: PolicyV2? = PolicyV2(),
+    @SerializedName("purpose") var purpose: String? = null,
+    @SerializedName("purposeDescription") var purposeDescription: String? = null,
+    @SerializedName("lawfulBasis") var lawfulBasis: String? = null,
+    @SerializedName("methodOfUse") var methodOfUse: String? = null,
+    @SerializedName("dpiaDate") var dpiaDate: String? = null,
+    @SerializedName("dpiaSummaryUrl") var dpiaSummaryUrl: String? = null,
+    @SerializedName("signature") var signature: SignatureV2? = SignatureV2(),
+    @SerializedName("active") var active: Boolean? = null,
+    @SerializedName("forgettable") var forgettable: Boolean? = null,
+    @SerializedName("compatibleWithVersionId") var compatibleWithVersionId: String? = null,
+    @SerializedName("lifecycle") var lifecycle: String? = null
+
+)
