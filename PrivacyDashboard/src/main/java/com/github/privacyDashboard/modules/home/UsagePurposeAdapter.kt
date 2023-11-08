d575a67c63176e32e200ac15f1a93a6c680dd9cb
milan <milan@igrant.io>
2023-11-08 19:49:06 +0530

    Fix: #190 - Added switch button bb consent

--- PrivacyDashboard/src/main/java/com/github/privacyDashboard/modules/home/UsagePurposeAdapter.kt
+++ PrivacyDashboard/src/main/java/com/github/privacyDashboard/modules/home/UsagePurposeAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.github.privacyDashboard.R
+import com.github.privacyDashboard.customView.SwitchButton
 import com.github.privacyDashboard.databinding.BbconsentItemUsagePurposeBinding
 import com.github.privacyDashboard.models.PurposeConsent
 
@@ -40,14 +41,15 @@ class UsagePurposeAdapter(
         var itemRowBinding: BbconsentItemUsagePurposeBinding
         fun bind(purposeConsent: PurposeConsent?, mListener: UsagePurposeClickListener) {
             itemRowBinding.ctvItemName.text = purposeConsent?.purpose?.name ?: ""
-            itemRowBinding.ctvStatus.text = if (purposeConsent?.count?.consented == 0) itemRowBinding.ctvStatus.context
-                .resources
-                .getString(R.string.bb_consent_dashboard_disallow) else itemRowBinding.ctvStatus.context
-                .resources.getString(
-                    R.string.bb_consent_dashboard_allow,
-                    purposeConsent?.count?.consented,
-                    purposeConsent?.count?.total
-                )
+            itemRowBinding.ctvStatus.text =
+                if (purposeConsent?.count?.consented == 0) itemRowBinding.ctvStatus.context
+                    .resources
+                    .getString(R.string.bb_consent_dashboard_disallow) else itemRowBinding.ctvStatus.context
+                    .resources.getString(
+                        R.string.bb_consent_dashboard_allow,
+                        purposeConsent?.count?.consented,
+                        purposeConsent?.count?.total
+                    )
             itemRowBinding.llDisable.visibility =
                 if (purposeConsent?.purpose?.lawfulUsage == true) View.VISIBLE else View.GONE
 
@@ -56,12 +58,15 @@ class UsagePurposeAdapter(
             itemRowBinding.switchButton.setOnCheckedChangeListener(null)
             itemRowBinding.switchButton.isChecked = purposeConsent?.count?.consented != 0
             itemRowBinding.switchButtonDisabled.isChecked = purposeConsent?.count?.consented != 0
-            itemRowBinding.switchButton.setOnCheckedChangeListener { view, isChecked ->
-                mListener.onSetStatus(
-                    purposeConsent,
-                    isChecked
-                )
-            }
+            itemRowBinding.switchButton.setOnCheckedChangeListener(object :
+                SwitchButton.OnCheckedChangeListener {
+                override fun onCheckedChanged(view: SwitchButton?, isChecked: Boolean) {
+                    mListener.onSetStatus(
+                        purposeConsent,
+                        isChecked
+                    )
+                }
+            })
             itemRowBinding.llItem.setOnClickListener { mListener.onItemClick(purposeConsent) }
         }
 
